124938596743036a6e53b06fed1862de31a8868d
Zhiyuanlu000217 <lu000217@algonquinlive.com>
2025-06-30 10:16:59 -0400

    Modify server

--- app.js
+++ app.js
@@ -9,24 +9,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Placeholder for future API endpoints that will be used in later demo stages:
 
-// @TODO: Future /api/users endpoint (for the SELECT * SQL anti-pattern demo)
-// app.get('/api/users', (req, res) => {
-//   // This endpoint will demonstrate SQL injection vulnerabilities
-//   // and the SELECT * anti-pattern in later demo stages
-// });
-
-// @TODO: Future /api/unstable endpoint (for the health probe demo's deliberate instability)
-// app.get('/api/unstable', (req, res) => {
-//   // This endpoint will be deliberately unstable for health probe testing
-//   // in later demo stages
-// });
-
-// @TODO: Future /api/stress endpoint (for the high-concurrency/performance demo)
-// app.get('/api/stress', (req, res) => {
-//   // This endpoint will be used for stress testing and performance optimization
-//   // in later demo stages
-// });
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`DevOps Demo App is running on http://localhost:${PORT}`);
